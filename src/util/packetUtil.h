584ca29e3bb89b8042d9da12d2e1992dde0666d1
Farhan Poharsae <grobot.developer@gmail.com>
2020-04-16 15:31:35 +0700

    change format on modbus packet

--- src/util/packetUtil.h
+++ src/util/packetUtil.h
@@ -2,14 +2,15 @@ extern FastCRC16 crc16;
 #ifndef SG_MCU_SENSOR_PACKETUTIL_H
 #define SG_MCU_SENSOR_PACKETUTIL_H
 
-uint16_t generatePacket(byte* packets, byte station, byte func, byte *data, uint8_t dataSize) {
+uint16_t generatePacket(byte *packets, byte station, byte func, byte *data, uint8_t dataSize) {
   packets[0] = station;
   packets[1] = func;
-  memcpy(&packets[2], data, dataSize);
-  uint16_t crc = crc16.modbus(packets, dataSize + 2);
-  packets[dataSize + sizeof(station) + sizeof(func)] = crc & 0xff;
-  packets[dataSize + sizeof(station) + sizeof(func) + 1] = crc >> 8;
-  return dataSize + sizeof(station) + sizeof(func) + 2;
+  packets[2] = dataSize;
+  memcpy(&packets[3], data, dataSize);
+  uint16_t crc = crc16.modbus(packets, dataSize + 3);
+  packets[dataSize + sizeof(station) + sizeof(func) + sizeof(dataSize)] = crc & 0xff;
+  packets[dataSize + sizeof(station) + sizeof(func) + sizeof(dataSize) + 1] = crc >> 8;
+  return sizeof(dataSize) + sizeof(station) + sizeof(func) + dataSize + sizeof(crc);
 }
 
 #endif
